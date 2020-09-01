0600a8846aaff5f30bcc1143f28d248136b22418
Mauro Sanhueza <mauro.sanhueza@walmart.com>
2020-09-01 16:31:32 -0400

    paredes

--- player/Player.gd
+++ player/Player.gd
@@ -9,5 +9,10 @@ func _ready():
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
-		print("derecha")
 		position.x += 1
+	if Input.is_action_pressed("move_left"):
+		position.x -= 1
+	if Input.is_action_pressed("move_up"):
+		position.y -= 1
+	if Input.is_action_pressed("move_down"):
+		position.y += 1
