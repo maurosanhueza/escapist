1e1be057c6b4b1884e2babf01e7de5676e784cdc
maurosanhueza <maurosanhueza@gmail.com>
2020-09-03 00:02:08 -0400

    bordes mas circuloas

--- player/Player.gd
+++ player/Player.gd
@@ -1,18 +1,28 @@
 extends KinematicBody2D
 class_name Player
 
-export var speed = 400  # How fast the player will move (pixels/sec).
+export var speed : int = 4  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	
 func _physics_process(delta):
-	if Input.is_action_pressed("move_right"):
-		position.x += 1
-	if Input.is_action_pressed("move_left"):
-		position.x -= 1
 	if Input.is_action_pressed("move_up"):
-		position.y -= 1
+		var movement : Vector2 = Vector2(0,-speed)
+		move_and_collide(movement)
+		#position.y -= speed	
+	if Input.is_action_pressed("move_right"):
+		var movement : Vector2 = Vector2(speed,0)
+		move_and_collide(movement)
+		#position.x += speed
 	if Input.is_action_pressed("move_down"):
-		position.y += 1
+		var movement : Vector2 = Vector2(0,speed)
+		move_and_collide(movement)
+		#position.y += speed		
+	if Input.is_action_pressed("move_left"):
+		var movement : Vector2 = Vector2(-speed,0)
+		move_and_collide(movement)
+		#position.x -= speed
+
+
