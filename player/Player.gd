31a395ae5c971cb76366a5dc61b90fdcd41b68bb
maurosanhueza <maurosanhueza@gmail.com>
2020-09-09 01:24:47 -0300

    sprites

--- player/Player.gd
+++ player/Player.gd
@@ -1,28 +1,48 @@
 extends KinematicBody2D
 class_name Player
 
-export var speed : int = 4  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
-func _ready():
-	screen_size = get_viewport_rect().size
-	
-func _physics_process(delta):
-	if Input.is_action_pressed("move_up"):
-		var movement : Vector2 = Vector2(0,-speed)
-		move_and_collide(movement)
-		#position.y -= speed	
-	if Input.is_action_pressed("move_right"):
-		var movement : Vector2 = Vector2(speed,0)
-		move_and_collide(movement)
-		#position.x += speed
-	if Input.is_action_pressed("move_down"):
-		var movement : Vector2 = Vector2(0,speed)
-		move_and_collide(movement)
-		#position.y += speed		
-	if Input.is_action_pressed("move_left"):
-		var movement : Vector2 = Vector2(-speed,0)
-		move_and_collide(movement)
-		#position.x -= speed
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const JUMP_HEIGHT = -500
 
+export var speed : int = 100
+export var movement : Vector2 = Vector2()
+	
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	movement.y += GRAVITY
+	
+	#if Input.is_action_pressed("move_up"):
+	#	movement = Vector2(0,-speed)
+	#	#$Player_sprite.flip_h = false
+	#	$Player_sprite.animation = "jump"
+		
+	if Input.is_action_pressed("move_right"):
+		#movement = Vector2(speed,0)
+		movement.x = speed
+		$Player_sprite.flip_h = false
+		$Player_sprite.animation = "walk"
+
+	#elif Input.is_action_pressed("move_down"):
+	#	movement = Vector2(0,speed)
+		
+	elif Input.is_action_pressed("move_left"):
+		#movement = Vector2(-speed,0)
+		movement.x = -speed
+		$Player_sprite.flip_h = true
+		$Player_sprite.animation = "walk"
+	else:
+		#movement = Vector2(0,400)
+		movement.x = 0
+		$Player_sprite.animation = "idle"
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("move_up"):
+			#movement = Vector2(0,-400)	
+			movement.y = JUMP_HEIGHT
+		
+	#move_and_collide(movement)
+	movement = move_and_slide(movement, UP)
 
