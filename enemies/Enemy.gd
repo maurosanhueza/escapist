5753a1e7b30ff7c509cba686985320bb6f9f9ef5
maurosanhueza <maurosanhueza@gmail.com>
2020-09-11 00:17:56 -0300

    mover entre escenas

--- enemies/Enemy.gd
+++ enemies/Enemy.gd
@@ -1,4 +1,14 @@
 extends KinematicBody2D
 
-func _ready():
-	pass # Replace with function body.
+const UP = Vector2(0,-1)
+const ACCELERATION = 70
+const GRAVITY = 20
+
+export var speed : int = 200
+export var movement : Vector2 = Vector2()
+
+func _physics_process(delta):
+	movement.y += GRAVITY
+	var friction = false
+
+	movement = move_and_slide(movement, UP)	
